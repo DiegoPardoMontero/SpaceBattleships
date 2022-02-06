ad1104dcf3545c5a4e71483862b6fac93570b2a5
Diego Pardo <diegopardomontero@gmail.com>
2022-02-06 16:28:43 -0500

    Agregada configuración de jugadores

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 WIDTH, HEIGTH = 900, 500 #Declaradas constantes de anchura y altura
 WIN = pygame.display.set_mode((WIDTH, HEIGTH)) #Creada ventana con ancho y alto
@@ -7,10 +8,24 @@ pygame.display.set_caption("First Game!") #Crear nombre de la ventana
 WHITE = (255, 255, 255)
 
 FPS = 60
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'spaceship_yellow.png'))
+
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+
+RED_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'spaceship_red.png'))
+
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
 def draw_window():
     WIN.fill((WHITE))
+    WIN.blit(YELLOW_SPACESHIP, (300, 100)) #Dibujar una superficie. Para poner texto o imágenes. 
+    WIN.blit(RED_SPACESHIP, (700, 100))
     pygame.display.update()
+
 
 def main():
     clock = pygame.time.Clock()
