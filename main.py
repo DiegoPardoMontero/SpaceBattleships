90378fc8d7829fd22349d3a4ce3d53605ee0955e
Diego Pardo <diegopardomontero@gmail.com>
2022-02-06 16:07:37 -0500

    Agregado color de fondo en la ventana

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 
 WIDTH, HEIGTH = 900, 500 #Declaradas constantes de anchura y altura
 WIN = pygame.display.set_mode((WIDTH, HEIGTH)) #Creada ventana con ancho y alto
+pygame.display.set_caption("First Game!") #Crear nombre de la ventana
+
+WHITE = (255, 255, 255)
 
 def main():
 
@@ -10,6 +13,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        WIN.fill((WHITE))
+        pygame.display.update()
 
     pygame.quit()
 
