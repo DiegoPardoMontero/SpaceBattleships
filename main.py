afb13ffca7c3d2a9dcc961d25d8985283fc2ee75
Diego Pardo <diegopardomontero@gmail.com>
2022-02-06 11:27:20 -0500

    Declaración medidas y pantalla

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import pygame
+
+WIDTH, HEIGTH = 900, 500 #Declaradas constantes de anchura y altura
+WIN = pygame.display.set_mode((WIDTH, HEIGTH)) #Creada ventana con ancho y alto
+
+def main():
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
